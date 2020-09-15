be80311d4af6496bcc27153624da477cc6117653
kukushkinnik <kukushkinnika@gmail.com>
2020-09-15 11:58:39 -0400

    added functionality to display image and fixed bug

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,22 @@
 let quotes = [
 
-    {image: '../images/oskar.jpg',
+    {image: 'C:/Users/Nik/Desktop/projects/random-quote/images/oscar.jpg',
     quote: '"Be yourself; everyone else is already taken." <br>-Oscar Wilde'
     },
 
-    {image: '../images/albert.jpg',
+    {image: 'C:/Users/Nik/Desktop/projects/random-quote/images/albert.jpg',
     quote: '"Two things are infinite: the universe and human stupidity; and I \'m not sure about the universe." <br>-Albert Einstein'
     },
 
-    {image: '../images/marcus.jpg',
+    {image: 'C:/Users/Nik/Desktop/projects/random-quote/images/marcus.jpg',
     quote: '"A room without books is like a body without a soul" <br>-Marcus Tullius Cicero'
     },
 
-    {image: '../images/mark.jpg',
+    {image: 'C:/Users/Nik/Desktop/projects/random-quote/images/mark.jpg',
     quote: '"If you tell the truth, you don\'t have to remember anything." <br>-Mark Twain'
     },
 
-    {image: '../images/friedrich.jpg',
+    {image: 'C:/Users/Nik/Desktop/projects/random-quote/images/friedrich.jpg',
     quote: '"Without music, life would be a mistake" <br>-Friedrich Nietzsche'
     }
 
@@ -25,9 +25,8 @@ let quotes = [
 function randomQuote () {
     var num = Math.floor(Math.random() * quotes.length);
     document.getElementById('quote-box').innerHTML = quotes[num].quote;
-  
-  
-};
+    document.getElementById('image-box').src = quotes[num].image;
+}
     
   
 
